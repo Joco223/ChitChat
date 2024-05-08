631ec3a20d12adbaa4046eeec2704082b28cef5f
Jovana Mihajlovic <jovanmihajlovic22@gmail.com>
2024-05-09 00:35:15 +0200

    Implemented login and register functionality, todo cleanup

---
+++ Helpers/SupabaseHandler.cs
@@ -0,0 +1,41 @@
+﻿using Supabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChitChat.Helpers
+{
+	public class SupabaseHandler
+	{
+		private string? supabaseUrl = "";
+		private string? supabaseKey = "";
+
+		private Client localClient;
+
+        public Client Client { get => localClient; set => localClient = value; }
+
+        private SupabaseHandler()
+		{
+			supabaseUrl = Properties.Settings.Default["supabaseUrl"].ToString();
+			supabaseKey = Properties.Settings.Default["supabaseKey"].ToString();
+
+			if (string.IsNullOrEmpty(supabaseUrl) || string.IsNullOrEmpty(supabaseKey))
+			{
+                throw new Exception("Supabase URL and Key must be set in the application settings");
+            }
+
+			// Initialize Supabase client
+			localClient = new Client(supabaseUrl, supabaseKey);
+            localClient.InitializeAsync().Wait();
+		}
+
+        private static readonly SupabaseHandler handler = new();
+
+		public static SupabaseHandler GetHandler()
+        {
+            return handler;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using ChitChat.Helpers;
+using Supabase.Gotrue;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,20 +18,85 @@ namespace ChitChat
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private bool registerMode = false;
+		private SupabaseHandler supabaseHandler = SupabaseHandler.GetHandler();
+
 		public MainWindow()
 		{
 			InitializeComponent();
-			
 		}
 
-		private void registerButton_Click(object sender, RoutedEventArgs e)
+		async private void registerButton_Click(object sender, RoutedEventArgs e)
 		{
-			usernameLabel.Visibility = Visibility.Visible;
-			username.Visibility = Visibility.Visible;
-			confirmPasswordLabel.Visibility = Visibility.Visible;
-			confirmPassword.Visibility = Visibility.Visible;
-			cancelRegistrationButton.Visibility = Visibility.Visible;
-			loginButton.Visibility = Visibility.Collapsed;
+			if (registerMode)
+			{
+				// Check if username, email, and password are not empty
+				if (string.IsNullOrEmpty(username.Text) || string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Password))
+				{
+					MessageBox.Show("Username, email, and password are required", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // Check if email is valid
+
+
+                // Register user
+				if (password.Password == confirmPassword.Password)
+				{
+					Dictionary<string, object> data = new()
+					{
+						{ "username", username.Text }
+					};
+
+					SignUpOptions options = new Supabase.Gotrue.SignUpOptions()
+					{ 
+						Data = data
+					};
+
+					try
+					{
+                        Session? session = await supabaseHandler.Client.Auth.SignUp(email.Text, password.Password, options);
+
+                        if (session != null)
+                        {
+                            MessageBox.Show("User registered successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                            usernameLabel.Visibility = Visibility.Collapsed;
+                            username.Visibility = Visibility.Collapsed;
+                            confirmPasswordLabel.Visibility = Visibility.Collapsed;
+                            confirmPassword.Visibility = Visibility.Collapsed;
+                            cancelRegistrationButton.Visibility = Visibility.Collapsed;
+                            loginButton.Visibility = Visibility.Visible;
+							email.Text = "";
+							password.Password = "";
+                            registerMode = false;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to register user", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+					catch (Exception ex)
+					{
+                        MessageBox.Show("Failed to register user, " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        throw;
+					}
+				}
+				else
+				{
+					MessageBox.Show("Passwords do not match", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+            }
+            else
+			{
+                // Show registration form
+				usernameLabel.Visibility = Visibility.Visible;
+				username.Visibility = Visibility.Visible;
+				confirmPasswordLabel.Visibility = Visibility.Visible;
+				confirmPassword.Visibility = Visibility.Visible;
+				cancelRegistrationButton.Visibility = Visibility.Visible;
+				loginButton.Visibility = Visibility.Collapsed;
+				registerMode = true;
+            }
         }
 
 		private void cancelRegistrationButton_Click(object sender, RoutedEventArgs e)
@@ -40,11 +107,25 @@ namespace ChitChat
             confirmPassword.Visibility = Visibility.Collapsed;
             cancelRegistrationButton.Visibility = Visibility.Collapsed;
             loginButton.Visibility = Visibility.Visible;
+			registerMode = false;
         }
 
-		private void loginButton_Click(object sender, RoutedEventArgs e)
+		async private void loginButton_Click(object sender, RoutedEventArgs e)
 		{
+			// Check if email and password are not empty
+			if (string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Password))
+			{
+				MessageBox.Show("Email and password are required", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
+            // Login user
+			Session? session = await supabaseHandler.Client.Auth.SignIn(email.Text, password.Password);
+
+			if (session != null)
+			{
+				MessageBox.Show("User logged in successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
